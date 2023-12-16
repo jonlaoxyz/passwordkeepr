36d2ffd374878041da39b240ce4e40123f65ae3f
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-12-16 17:09:56 -0600

    Function to createPasswordElement created

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,64 +1,26 @@
 // Client facing scripts here
 
 // Creating new password
-const createPasswordElement = function (passwordObj) {
-
-  const $tweet = $(`
- <article class="tweet">
-        <header>
-          <img src="${tweetObj.user.avatars}" alt="Profile Image">
-          <div>
-            <p>${tweetObj.user.name}</p>
-            <p id="tweet-id"><b>${tweetObj.user.handle}</b></p>
-          </div>
-        </header>
-      
-        <div class="tweet-content">
-          <p >${$("<div>").text(tweetObj.content.text).html()}</p>
-        </div>
-      
-        <footer>
-          <p><b>${timeAgo}</b></p>
-          <div class="icons">
-            <i class="fa-solid fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
-          </div>
-        </footer>
-      </article>
- `);
-
-  return $tweet;
-};
-
+const createPasswordElement = function(passwordObj) {
+  const $passwordRow = $(`
     <tr>
-      <td>Example Website</td>
-      <td>john_doe</td>
-      <td>********</td>
-      <td>http://example.com</td>
-      <td>General</td>
+      <td>${passwordObj.website_name}</td>
+      <td>${passwordObj.username}</td>
+      <td>${passwordObj.password}</td>
+      <td>${passwordObj.url}</td>
+      <td>${passwordObj.category}</td>
       <td>
         <span class="edit">Edit</span> /
         <span class="delete">Delete</span>
       </td>
     </tr>
+  `);
+
+  return $passwordRow;
+};
+
+    
 
 
 
 
-<table>
-  <thead>
-    <tr>
-      <th>Website Name</th>
-      <th>Username</th>
-      <th>Password</th>
-      <th>URL</th>
-      <th>Category</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    <!-- Script for container -->
-  </tbody>
-</table>
