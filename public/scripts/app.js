4991ba36769ae0f99595e2a9c223208052a9b9d0
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-12-18 10:49:51 -0600

    zone identifier image deleted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,23 @@
 // Client facing scripts here
 
 // Creating new password
-// const createPasswordElement = function(passwordObj) {
-//   const $passwordRow = $(`
-//     <tr>
-//       <td>${passwordObj.website_name}</td>
-//       <td>${passwordObj.username}</td>
-//       <td>${passwordObj.password}</td>
-//       <td><a href="">${passwordObj.url}</a></td>
-//       <td>${passwordObj.category}</td>
-//       <td>
-//         <i class="far fa-edit" style="color: #989ea9;"></i> /
-//         <i class="fa-solid fa-trash" style="color: #989ea9;"></i>
-//       </td>
-//     </tr>
-//   `);
+const createPasswordElement = function(passwordObj) {
+  const $passwordRow = $(`
+    <tr>
+      <td>${passwordObj.website_name}</td>
+      <td>${passwordObj.username}</td>
+      <td>${passwordObj.password}</td>
+      <td><a href="">${passwordObj.url}</a></td>
+      <td>${passwordObj.category}</td>
+      <td>
+        <i class="far fa-edit" style="color: #989ea9;"></i> /
+        <i class="fa-solid fa-trash" style="color: #989ea9;"></i>
+      </td>
+    </tr>
+  `);
 
-//   return $passwordRow;
-// };
+  return $passwordRow;
+};
 
     
 
