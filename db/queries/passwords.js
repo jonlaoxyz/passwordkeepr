16151d48d7617823e36aeb14b8f04cf16a5deac1
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-12-18 19:41:25 -0600

    need to commit to go to feature branch

--- db/queries/passwords.js
+++ db/queries/passwords.js
@@ -7,4 +7,17 @@ const getPasswords = () => {
     });
 };
 
-module.exports = { getPasswords };
+const createPassword = (website_name, username, password, url, category_id, organization_id) => {
+  return db.query(`
+    INSERT INTO passwords(website_name, username, password, url, category_id, organization_id)
+    VALUES($1, $2, $3, $4, $5, $6)
+    RETURNING *
+  `, [website_name, username, password, url, category_id, organization_id])
+    .then(data => {
+      console.log("yo there!")
+      return data.rows[0]; // Assuming you expect only one row to be returned
+    });
+};
+
+
+module.exports = { getPasswords , createPassword};
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,25 +1,50 @@
 // Client facing scripts here
 
 // Creating new password
-const createPasswordElement = function(passwordObj) {
-  const $passwordRow = $(`
-    <tr>
-      <td>${passwordObj.website_name}</td>
-      <td>${passwordObj.username}</td>
-      <td>${passwordObj.password}</td>
-      <td><a href="">${passwordObj.url}</a></td>
-      <td>${passwordObj.category}</td>
-      <td>
-        <i class="far fa-edit" style="color: #989ea9;"></i> /
-        <i class="fa-solid fa-trash" style="color: #989ea9;"></i>
-      </td>
-    </tr>
+const createPasswordElement = function (passwordObj) {
+  const $passwordContainer = $(`
+  <article class="password-item">
+  <div class="website-name">
+    <span class="col">Website:</span>
+    <span class="col">${passwordObj.website_name}</span>
+  </div>
+  <div class="user-id">
+    <span class="col">User Name:</span>
+    <span class="col">${passwordObj.username}</span>
+    <span class="col"><i class="bi bi-copy"></i></span>
+  </div>
+  <div class="password">
+    <span>Password:</span>
+    <span>${passwordObj.password}</span>
+    <span><i class="bi bi-copy"></i></span>
+  </div>
+  <div class="url">
+    <span>URL:</span>
+    <span><a href="${passwordObj.url}">${passwordObj.url}</a></span>
+  </div>
+  <div class="category">
+    <span>Category:</span>
+    <span>${passwordObj.category}</span>
+  </div>
+  <div class="actions">
+      <i class="bi bi-pencil-square"></i> <i class="bi bi-trash3"></i>
+  </div>
+</article>
   `);
 
-  return $passwordRow;
+  return $passwordContainer;
 };
 
-    
+// Append new password to the password-container
+const renderPasswords = function (passwords) {
+  $('#password-container').empty();
+  for (const password of passwords) {
+    const $password = createPasswordElement(password);
+
+    $('#tweets-container').prepend($tweet);
+  }
+
+};
 
 
 
