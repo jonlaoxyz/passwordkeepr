4b10b57929221bc110c09468fd45447874fc511f
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-12-19 00:04:37 -0600

    copy functionality implemented

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,32 +4,32 @@
 const createPasswordElement = function (passwordObj) {
   const $passwordContainer = $(`
   <section class="password-item rounded shadow">
-          <div class="website-name row">
-            <h2>${passwordObj.website_name}</h2>
-          </div>
-          <div class="user-id row">
-            <span class="col-2 p-1 text-end">User Name:</span>
-            <span class="col p-1">${passwordObj.username}</span>
-            <span class="col p-1"><a href=""><i class="bi bi-copy"></i></a></span>
-          </div>
-          <div class="password row">
-            <span class="col-2 p-1 text-end">Password:</span>
-            <span class="col p-1">${passwordObj.password}</span>
-            <span class="col p-1"><a href=""><i class="bi bi-copy"></i></a></span>
-          </div>
-          <div class="url row">
-            <span class="col-2 p-1 text-end">URL:</span>
-            <span class="col p-1"><a href="${passwordObj.url}">${passwordObj.url}</a></span>
-          </div>
-          <div class="category row">
-            <span class="col-2 p-1 text-end">Category:</span>
-            <span class="col p-1">${passwordObj.category_name}</span>
-          </div>
-          <div class="actions text-end">
-            <a href="" title="Edit"><i class="bi bi-pencil-square"></i></a> |
-            <a href="" title="Delete"><i class="bi bi-trash3"></i></a>
-          </div>
-       </section>
+      <div class="website-name row">
+        <h2>${passwordObj.website_name}</h2>
+      </div>
+      <div class="user-id row">
+        <span class="col-2 p-1 text-end">User Name:</span>
+        <span class="col p-1">${passwordObj.username}</span>
+        <span class="col p-1"><a href="" class="copy-icon" data-text="${passwordObj.username}"><i class="bi bi-copy"></i></a></span>
+      </div>
+      <div class="password row">
+        <span class="col-2 p-1 text-end">Password:</span>
+        <span class="col p-1">${passwordObj.password}</span>
+        <span class="col p-1"><a href="" class="copy-icon" data-text="${passwordObj.password}"><i class="bi bi-copy"></i></a></span>
+      </div>
+      <div class="url row">
+        <span class="col-2 p-1 text-end">URL:</span>
+        <span class="col p-1"><a href="${passwordObj.url}">${passwordObj.url}</a></span>
+      </div>
+      <div class="category row">
+        <span class="col-2 p-1 text-end">Category:</span>
+        <span class="col p-1">${passwordObj.category_name}</span>
+      </div>
+      <div class="actions text-end">
+        <a href="" title="Edit"><i class="bi bi-pencil-square"></i></a> |
+        <a href="" title="Delete"><i class="bi bi-trash3"></i></a>
+      </div>
+    </section>
   `);
 
   return $passwordContainer;
@@ -37,13 +37,21 @@ const createPasswordElement = function (passwordObj) {
 
 
 // Append new password to the password-container
-const renderPasswords = function(passwords) {
+const renderPasswords = function (passwords) {
   $('#password-list').empty();
   for (const password of passwords) {
     const $password = createPasswordElement(password);
 
     $('#password-list').append($password);
   }
+
+  // Add click event for copy-icon
+  $('.copy-icon').click(function (event) {
+    event.preventDefault();   //prevents scrolling to the top of the page
+    const textToCopy = $(this).data('text');
+    copyToClipboard(textToCopy);
+    alert("Copy Successfull");
+  });
 
 };
 
@@ -56,16 +64,54 @@ const loadPasswords = function () {
   });
 };
 
-$(document).ready(function () {
 
+
+$(document).ready(function () {
   // Load passwords and rendering.
   loadPasswords().then((response) => {
     const passwords = response.passwordsCategoriesJoin; // Access the array property
     renderPasswords(passwords);
   }).catch(function (error) {
-    console.error("Error loading passwords:", error);
+    console.error('Error loading passwords:', error);
   });
+});
+
+
+
+  // Post tweet details to server when Tweet is submitted
+  // $("#form").on("submit", function(event) {
+  //   event.preventDefault();
+
+  //   //validation
+
+  //   //If everything goes great, proceed with sending tweet to server
+  //   const formData = $(event.currentTarget).serialize();
+  //   console.log(formData);
+  //   $.ajax({
+  //     method: "POST",
+  //     url: "/api",
+  //     data: formData
+  //   }).then(() => {
+  //     loadTweets().then((tweets) => {
+  //       $(".counter").text("140");
+  //       // post success message
+  //       let SuccessMessage = $(".success-message");
+  //       SuccessMessage.text("Tweet Successfully Posted!").show();
+  //       setTimeout(() => {
+  //         SuccessMessage.hide();
+  //       }, 2500);
+  //       // render tweets
+  //       renderTweets(tweets);
+  //     }).catch(function(error) {
+  //       console.error("Error loading tweets:", error);
+  //     });
+
+  //     $tweetText.val("");
+  //   });
+
+
+  // });
 
   
 
-});
+
---
+++ public/scripts/copyToClipboard.js
@@ -0,0 +1,9 @@
+// Copy text to clipboard
+const copyToClipboard = function (text) {
+  const textArea = document.createElement('textarea');
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+};
