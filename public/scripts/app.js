bcbe7b030539ecfc5f19b40fb0daad978fbcaf2a
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-12-18 21:45:44 -0600

    diplayed list of saved passwords

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,49 +3,69 @@
 // Creating new password
 const createPasswordElement = function (passwordObj) {
   const $passwordContainer = $(`
-  <article class="password-item">
-  <div class="website-name">
-    <span class="col">Website:</span>
-    <span class="col">${passwordObj.website_name}</span>
-  </div>
-  <div class="user-id">
-    <span class="col">User Name:</span>
-    <span class="col">${passwordObj.username}</span>
-    <span class="col"><i class="bi bi-copy"></i></span>
-  </div>
-  <div class="password">
-    <span>Password:</span>
-    <span>${passwordObj.password}</span>
-    <span><i class="bi bi-copy"></i></span>
-  </div>
-  <div class="url">
-    <span>URL:</span>
-    <span><a href="${passwordObj.url}">${passwordObj.url}</a></span>
-  </div>
-  <div class="category">
-    <span>Category:</span>
-    <span>${passwordObj.category}</span>
-  </div>
-  <div class="actions">
-      <i class="bi bi-pencil-square"></i> <i class="bi bi-trash3"></i>
-  </div>
-</article>
+  <section class="password-item rounded shadow">
+          <div class="website-name row">
+            <h2>${passwordObj.website_name}</h2>
+          </div>
+          <div class="user-id row">
+            <span class="col-2 p-1 text-end">User Name:</span>
+            <span class="col p-1">${passwordObj.username}</span>
+            <span class="col p-1"><a href=""><i class="bi bi-copy"></i></a></span>
+          </div>
+          <div class="password row">
+            <span class="col-2 p-1 text-end">Password:</span>
+            <span class="col p-1">${passwordObj.password}</span>
+            <span class="col p-1"><a href=""><i class="bi bi-copy"></i></a></span>
+          </div>
+          <div class="url row">
+            <span class="col-2 p-1 text-end">URL:</span>
+            <span class="col p-1"><a href="${passwordObj.url}">${passwordObj.url}</a></span>
+          </div>
+          <div class="category row">
+            <span class="col-2 p-1 text-end">Category:</span>
+            <span class="col p-1">${passwordObj.category_name}</span>
+          </div>
+          <div class="actions text-end">
+            <a href="" title="Edit"><i class="bi bi-pencil-square"></i></a> |
+            <a href="" title="Delete"><i class="bi bi-trash3"></i></a>
+          </div>
+       </section>
   `);
 
   return $passwordContainer;
 };
 
+
 // Append new password to the password-container
-const renderPasswords = function (passwords) {
-  $('#password-container').empty();
+const renderPasswords = function(passwords) {
+  $('#password-list').empty();
   for (const password of passwords) {
     const $password = createPasswordElement(password);
 
-    $('#tweets-container').prepend($tweet);
+    $('#password-list').append($password);
   }
 
 };
 
+// Function to loads a password from '/api/passwordsCategoriesJoin' and recieve an array of tweets in json
+const loadPasswords = function () {
+  return $.ajax({
+    method: "GET",
+    url: "/api/passwordsCategoriesJoin",
+    dataType: "json"
+  });
+};
 
+$(document).ready(function () {
 
+  // Load passwords and rendering.
+  loadPasswords().then((response) => {
+    const passwords = response.passwordsCategoriesJoin; // Access the array property
+    renderPasswords(passwords);
+  }).catch(function (error) {
+    console.error("Error loading passwords:", error);
+  });
 
+  
+
+});
