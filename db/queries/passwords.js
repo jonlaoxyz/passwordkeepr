45ef6c19c8617295a59f878d3cc670a6d0a1d3e0
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-12-18 20:39:01 -0600

    db/queries/passwords.js error resolved

--- db/queries/passwords.js
+++ db/queries/passwords.js
@@ -19,18 +19,5 @@ const createPassword = (website_name, username, password, url, category_id, orga
 };
 
 
-module.exports = { getPasswords , createPassword};
-const createPassword = (website_name, username, password, url, category_id, organization_id) => {
-  return db.query(`
-    INSERT INTO passwords(website_name, username, password, url, category_id, organization_id)
-    VALUES($1, $2, $3, $4, $5, $6)
-    RETURNING *
-  `, [website_name, username, password, url, category_id, organization_id])
-    .then(data => {
-      console.log("yo there!")
-      return data.rows[0]; // Assuming you expect only one row to be returned
-    });
-};
-
 
 module.exports = { getPasswords , createPassword};
